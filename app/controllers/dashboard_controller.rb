2c53cef7e9c2202fad9e29803ed86302cf3957fa
Christopher Kalafarski <farski@Mideel.local>
2014-12-31 01:00:00 -0500

    Better graph lines

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -10,7 +10,7 @@ class DashboardController < ApplicationController
       data = []
       distance_t = 0
 
-      activities = user.activities.runs.order(start_date: :asc).where(['start_date >= ?', Time.new(2014, 12, 1)]).where(['start_date < ?', Time.new(2015, 5, 1)])
+      activities = user.activities.runs.p2015.order(start_date: :asc)
 
       for activity in activities
         distance_t += (activity[:distance] / 1000)
