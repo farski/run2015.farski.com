8d865262d9c24c133f72ea636ffeba945dbba0d3
Chris Kalafarski <chris@farski.com>
2015-11-07 15:07:36 -0500

    Disable some routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,13 @@ Rails.application.routes.draw do
 
   root to: 'dashboard#index'
 
-  get 'leaderboard' => 'dashboard#leaderboard'
-  get 'graph' => 'graphs#rickshaw'
-  get 'stats' => 'dashboard#stats'
+  # get 'leaderboard' => 'dashboard#leaderboard'
+  # get 'graph' => 'graphs#rickshaw'
+  # get 'stats' => 'dashboard#stats'
   get 'signup' => 'dashboard#index'
 
-  get 'import/update' => 'import#update'
-  get 'update' => 'import#update'
+  # get 'import/update' => 'import#update'
+  # get 'update' => 'import#update'
 
   get 'activities' => 'activities#index'
 
