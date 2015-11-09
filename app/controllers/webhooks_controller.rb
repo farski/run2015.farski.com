35152351e7096ba1b24bef1a4c2c602276b13c0e
Chris Kalafarski <chris@farski.com>
2015-11-09 15:02:18 -0500

    Fix webhook verify params

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,12 +1,13 @@
 class WebhooksController < ApplicationController
   def get
+    mode = params['hub.mode']
     challenge = params['hub.challenge']
-    token = params['verify_token']
+    token = params['hub.verify_token']
 
-    if token == '0809756ca4e9bac126e09e3613dfe665'
-      render status: 200, text: challenge
-    else
+    if mode != 'subscribe' || token != '0809756ca4e9bac126e09e3613dfe665'
       render status: 400, text: nil
+    else
+      render status: 200, text: challenge
     end
   end
 
